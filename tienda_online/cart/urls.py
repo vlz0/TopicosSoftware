d933ead92d2c1b5b083627a4fc0483c0d8d8f553
sgalle16 <sgalle16@eafit.edu.co>
2024-03-19 11:02:36 -0500

    add payment integration  stripe checkout

--- tienda_online/cart/urls.py
+++ tienda_online/cart/urls.py
@@ -1,16 +1,17 @@
-
-
-from django.conf.urls.static import static 
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path,include 
-from . import views 
+from django.urls import path, include
+from . import views
+
+from .views import CreateCheckoutSessionView, SuccessView
 
 urlpatterns = [
-    
-    path('add-to-cart/',views.add_to_cart,name="add-to-cart"),  
-    path('delete-product/',views.cart_delete,name="delete-product"),   
-    path('update-cart/',views.update_cart,name="update-cart"),
-    path('cart_view/',views.cart_view,name="cart_view"),
 
+    path('add-to-cart/', views.add_to_cart, name="add-to-cart"),
+    path('delete-product/', views.cart_delete, name="delete-product"),
+    path('update-cart/', views.update_cart, name="update-cart"),
+    path('cart_view/', views.cart_view, name="cart_view"),
+
+    path('create-checkout-session/', CreateCheckoutSessionView.as_view(), name='create-checkout-session'),
+    path('success/', SuccessView.as_view(), name='success'),
 ]
-
--- tienda_online/tienda_online/settings.py
+++ tienda_online/tienda_online/settings.py
@@ -10,26 +10,28 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path 
+from pathlib import Path
 import os
-import environ 
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-env = environ.Env()
-environ.Env.read_env()
+
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c-1y-m+*w&!sm502_*wdfbn5z8^*308!sxyt5nl+od(+p3b607'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-c-1y-m+*w&!sm502_*wdfbn5z8^*308!sxyt5nl+od(+p3b607')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
-
+# Allowed hosts
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 # Application definition
 
@@ -39,11 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles', 
-    'tienda',  
+    'django.contrib.staticfiles',
+    'tienda',
     'crispy_forms',
-    'crispy_bootstrap4', 
-    'cart'
+    'crispy_bootstrap4',
+    'cart',
+
 ]
 
 MIDDLEWARE = [
@@ -64,12 +67,12 @@ TEMPLATES = [
         'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
-            'context_processors': [ 
+            'context_processors': [
                 'cart.procesador.importe_total_carro',
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages', 
+                'django.contrib.messages.context_processors.messages',
                 'cart.context_processors.cart',
             ],
         },
@@ -124,16 +127,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'  
-STATICFILES_DIRS=['static/']
+STATIC_URL = 'static/'
+STATICFILES_DIRS = ['static/']
 
-MEDIA_URL='media/' 
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 
+# Crispy forms
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+
+# Stripe API keys
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
