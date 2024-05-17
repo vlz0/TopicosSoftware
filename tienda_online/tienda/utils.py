f4b4305b153c4ced6816f7f8a2f0d82be6008c38
Juan José Villada Calle <88852332+jvillada1@users.noreply.github.com>
2024-05-17 12:18:47 -0500

    Update utils.py

--- tienda_online/tienda/utils.py
+++ tienda_online/tienda/utils.py
@@ -1,8 +1,18 @@
 from django.core.files.storage import default_storage
 from .interfaces import ImageStorage  
-
+import requests
 class ImageLocalStorage(ImageStorage):
     def store(self, image):
         if image:
             file_name = default_storage.save('uploads/product/' + image.name, image)
-            return file_name
+            return file_name 
+
+
+
+def get_allied_brands():
+    api_url = "https://run.mocky.io/v3/12470a11-9dfd-417e-bcc1-a85ff02d61ce"  
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
