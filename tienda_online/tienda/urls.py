f984be4655296685508b0e0a010f1f24e27cf65c
Juan José Villada Calle <88852332+jvillada1@users.noreply.github.com>
2024-05-17 12:22:58 -0500

    Update urls.py

--- tienda_online/tienda/urls.py
+++ tienda_online/tienda/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views 
-from .views import VRegistro, ProductSearchListView, ProductListAPI,AlianzaAPIView
+from .views import VRegistro, ProductSearchListView, ProductListAPI,AlliedBrandsView
 
 urlpatterns = [
     path('', views.home,name='home'),  
@@ -13,5 +13,5 @@ urlpatterns = [
 
     path('search/', ProductSearchListView.as_view(), name='search'),
     path('api/products/', ProductListAPI.as_view(), name='product-list-api'), 
-    path('api/alianza/', AlianzaAPIView.as_view(), name='api_alianza'),
+    path('allied-brands/', AlliedBrandsView.as_view(), name='allied_brands'),
 ]
